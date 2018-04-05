ae70aede96532935f4a35707e4d1a59aac3d060b
moore54 <moorekevin711@gmail.com>
2018-04-05 12:34:53 -0600

    Tying to change mass to discover relations

--- Julia_Dynamics/STOL_opt.jl
+++ Julia_Dynamics/STOL_opt.jl
@@ -5,34 +5,48 @@ using Dierckx
 using Roots
 using PyPlot
 PyPlot.close("all")
+rc("figure", figsize=(6.5, 4.6))
+rc("font", size=10.0)
+rc("lines", linewidth=1.5)
+rc("lines", markersize=3.0)
+rc("legend", frameon=false)
+rc("axes.spines", right=false, top=false)
+rc("figure.subplot", left=0.17, bottom=0.18, top=0.97, right=.69)
+rc("axes", color_cycle=["348ABD", "A60628", "009E73", "7A68A6", "D55E00", "CC79A7"])
+
 
 include("$fileLoc/STOL_dynamics_delta_e.jl")
 
 function objcon(x)
-    args = (ds,h_set,dt,PW,optimize,P)
+    args = (ds,h_set,dt,TW,optimize,P)
     # println("1")
     return obj(x,args...)
 end
 
-function obj(delta_e,ds,h_set,dt,PW,optimize,P)
+function obj(x,ds,h_set,dt,TW,optimize,P)
 
     #Reinitizilze the integrator
     integ_1 = integrator(ds)
+    dt = x[1]
+    delta_e = x[2:21]
+
 
     errorsum = 0
     savestates = zeros(length(delta_e),7)
     Va = zeros(delta_e)
     C_minheight = zeros(delta_e)
     C_vel = zeros(delta_e)
+    C_turnback = zeros(delta_e)
     CL = zeros(delta_e)
+    alpha_save = zeros(delta_e)
     for i = 1:length(delta_e)
-
         integ_1.p[:delta_e] = delta_e[i]*pi/180
-        integ_1.p[:delta_t] = 0.5 #thrust
-        try
-            integ_1.p[:thrust_weight] = PW/Va[i-1]
-        catch
-            integ_1.p[:thrust_weight] = PW/integ_1.p[:Va0]
+        # integ_1.p[:delta_t] = 0.5 #thrust
+
+        if i>1
+            integ_1.p[:thrust_weight] = TW #PW/Va[i-1]
+        else
+            integ_1.p[:thrust_weight] = TW #PW/integ_1.p[:Va0]
         end
 
         step!(integ_1,dt, true)
@@ -43,29 +57,35 @@ function obj(delta_e,ds,h_set,dt,PW,optimize,P)
         savestates[i,1:6] = integ_1.u
         savestates[i,7] = thrust
 
-        alpha = atan2(integ_1.u[3],integ_1.u[4])
+        alpha = atan2(integ_1.u[4],integ_1.u[3])
         sigma_alpha = (1 + exp(-P[:M]*(alpha-P[:alpha0])) + exp(P[:M]*(alpha+P[:alpha0])))/((1+exp(-P[:M]*(alpha-P[:alpha0])))*(1+exp(P[:M]*(alpha+P[:alpha0]))));
         CL[i] = (1-sigma_alpha)*(P[:C_L_0] + P[:C_L_alpha]*alpha) + sigma_alpha*(2*sign(alpha)*(sin(alpha)^2)*cos(alpha));
-
+        alpha_save[i] = alpha*180/pi
         c_vel = integ_1.p[:Va0]-Va[i]
 
         k = P[:ground_threshold]
         x0 = pi*e/k
         C_vel[i] = c_vel/(1+e^(-k*(-integ_1.u[2]-x0))) # set to zero if on ground, but smoothly
+        if i>1
+            C_turnback[i] = savestates[i-1,1]-savestates[i,1]
+        else
+            C_turnback[i] = -savestates[i,1]
+        end
         # println("2")
     end
 
 
-    J = errorsum/100
+    J = savestates[end,1]/10#errorsum/100
     C_minheight = 0.0-(-savestates[:,2])
-    C = [C_vel/10;C_minheight]
+    C_maxheight = h_set - -savestates[end,2]
+    C = [C_maxheight/h_set;C_vel/10;C_minheight;C_turnback/10]
     # println("3")
     # println(sqrt(integ_1.u[3]^2+integ_1.u[4]^2))
 
     if optimize
         return J,C,false
     else
-        return errorsum,savestates,C_vel,C_minheight,CL
+        return errorsum,savestates,C_maxheight,C_vel,C_minheight,CL,alpha_save
     end
 end
 
@@ -76,17 +96,31 @@ x0  = [P[:pn0],P[:pd0],P[:u0],P[:w0],P[:theta0],P[:q0]]
 
 # Set up ContinuousDynamicalSystem
 ds = ContinuousDynamicalSystem(testUAV!, x0, P)
-# integ_1 = integrator(ds)
-# step!(integ_1,dt, true)
-# println(integ_1.u)
+#Reinitizilze the integrator
 
-delta_e = [-64.1588, -85.0, 84.6705, -67.8829, 40.7014, -15.1621, -43.0495, 2.08015, -42.6057, 4.12959, -55.8678, 85.0, -26.2024, -9.43524, -75.5494, 32.4107, -19.205, 17.8462, -85.0, -81.844]
+delta_e = [-85.0, 1.16896, 52.1414, -32.6719, -2.07146, -15.4012, -9.35701, -11.8722, -10.8659, -11.2834, -11.2427, -10.1897, -12.6121, -9.07863, -13.8728, -6.67249, -19.487, 9.64414, -85.0, -85.0]
+# PW_var = [5.0,50.0, 50.0, 50.0, 50.0, 50.0, 50.0, 50.0, 50.0, 50.0, 50.0, 50.0, 50.0, 50.0, 24.1828, 11.2694, 10.3522, 39.4638, 9.19733, 15.5779]
+# delta_e = ones(20)*.5
 h_set = 10.0
+h_M = 10.0/13.5
 dt = .10
+TW = 0.8
+
+
+
 time_sim = collect(dt:dt:dt*length(delta_e))
-x0 = delta_e
-lb = ones(delta_e)*-85
-ub = ones(delta_e)*85
+
+lb_de = ones(delta_e)*-85
+ub_de = ones(delta_e)*85
+
+
+
+# x0 = [-85.0, 38.9728, -6.79026, 4.81065, 1.10381, -20.7914, -9.42534, -10.6752, -9.40374, -11.4258, -9.76186, -6.14866, -10.7634, -12.9517, -16.3083, -8.30803, -13.2473, 20.5823, -79.232, -85.0, 1.86897, 0.465348, 7.67862, 10.0, 10.0, 10.0, 10.0, 10.0, 9.91234, 9.15937, 5.05204, 0.568162, 0.335101, 2.02494, 9.59265, 10.0, 10.0, 10.0, 10.0, 10.0]
+# x0 = [-85.0, 38.5752, -5.39709, 3.44675, 2.27539, -21.6652, -7.49748, -10.5739, -13.0014, -7.24735, -14.1903, -8.88464, -11.6929, -9.35434, -12.2483, -7.6421, -20.109, 16.0491, -85.0, -85.0, 1.93352, 2.3695, 8.0, 8.0, 8.0, 8.0, 8.0, 8.0, 8.0, 8.0, 8.0, 8.0, 8.0, 8.0, 8.0, 8.0, 8.0, 8.0, 8.0, 8.0]
+# x0 = [.1,-84.5883, 71.655, -83.1494, 71.6458, -39.0269, 3.09616, -15.8954, -12.7499, -4.09139, -18.0408, -8.04835, -3.37305, -26.406, 7.02331, -23.5117, -7.31014, 7.87563, -23.9456, 60.2123, -85.0, 0.381699, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4]
+x0 = [.1;delta_e]
+lb = [1E-3;lb_de]
+ub = [5.0;ub_de]
 # println("4")
 # ----- Define Optimizer Options ----- #
 options = Dict{String, Any}()
@@ -97,7 +131,7 @@ options["Derivative level"] = 0
 options["Iterations limit"] = 1e8
 options["Major iterations limit"] = 1000
 options["Minor iterations limit"]= 1e8
-options["Major optimality tolerance"] = 1e-5 #Should be scaled so it is optimal with a solid 2 digits
+options["Major optimality tolerance"] = 1e-4 #Should be scaled so it is optimal with a solid 2 digits
 options["Minor optimality  tolerance"] = 1e-6
 options["Major feasibility tolerance"] = 1e-5
 options["Minor feasibility tolerance"] = 1e-6
@@ -107,89 +141,129 @@ options["Scale option"] = 1
 options["Scale tolerance"] = .95
 
 # PW_array = [.3,.5,.7,.8,.9,.999]#linspace(.459,.46,10)
-PW_array = linspace(10,20,5)
-PW = []
+Mass_array = [5.0,10.0,15.0]
+# args = []
+args2 = []
 optimize = []
-errorsum = zeros(PW_array)
-mean_thrust2weight = zeros(PW_array)
-total_energy = zeros(PW_array)
-final_dist = zeros(PW_array)
+errorsum = zeros(Mass_array)
+mean_thrust2weight = zeros(Mass_array)
+total_energy = zeros(Mass_array)
+final_dist = zeros(Mass_array)
 
 function zero_height(x)
-    return  zero_height(x,args...)
+    return  zero_height(x,args2...)
 end
 
 function zero_height(x,alt_spl,h_set)
     h = alt_spl(x)
     return h-h_set
 end
+testrun = false
+for i = 1:length(Mass_array)
+# i = 1
 
-for i = 1:length(PW_array)
-    # i = 1
+    P[:mass] = Mass_array[i]
+    Mass = Mass_array[i]
+    # h_set = h_M*Mass
+    if !testrun
+        optimize = true
 
-    optimize = true
-    PW = PW_array[i]
-    # println("5")
-    xopt, fopt, optinfo = snopt(objcon, x0, lb, ub, options;printfile = "$(fileLoc)/snopt-print$PW.out", sumfile = "$(fileLoc)/snopt-summary.out") #!!!!!! ARGS are in objcon wrapper function !!!!!!!!#
 
-    println("xopt: $(xopt)")
-    println("####################################################")
-    println(optinfo)
-    println("####################################################")
+        xopt, fopt, optinfo = snopt(objcon, x0, lb, ub, options;printfile = "$(fileLoc)/snopt-print$(Mass_array[i]).out", sumfile = "$(fileLoc)/snopt-summary.out") #!!!!!! ARGS are in objcon wrapper function !!!!!!!!#
 
+        println("xopt: $(xopt)")
+        println("####################################################")
+        println(optinfo)
+        println("####################################################")
+    else
+        xopt = x0
+    end
     optimize = false
 
-    delta_e = xopt
-    errorsum[i],savestates,C_vel,C_minheight,CL = objcon(delta_e)
+
+    errorsum[i],savestates,C_maxheight,C_vel,C_minheight,CL,alpha_save = objcon(xopt)
     mean_thrust2weight[i] = mean(savestates[:,end]./(ones(time_sim)*P[:mass]*P[:gravity]))
     Va = sqrt.(savestates[:,3].^2+savestates[:,4].^2)
 
     figname = "d_CL"
     PyPlot.figure(figname)
-    PyPlot.plot(savestates[:,1],CL,".-",label = "$(round.(PW,4))")
+    PyPlot.plot(savestates[:,1],CL,".-",label = "$(round.(Mass,4))")
     PyPlot.xlabel("pn (m)")
     PyPlot.ylabel("CL")
-    legend(loc="center left", title = "Power/Weight",bbox_to_anchor=(1, 0.5))
-    PyPlot.savefig("./figures/dynamic_opt/$figname.png",transparent = true)
+    legend(loc="center left", title = "Mass (kg)",bbox_to_anchor=(1, 0.5))
+    PyPlot.savefig("./figures/changing_mass/$figname.png",transparent = true)
+
+    figname = "d_q"
+    PyPlot.figure(figname)
+    PyPlot.plot(savestates[:,1],savestates[:,6],".-",label = "$(round.(Mass,4))")
+    PyPlot.xlabel("pn (m)")
+    PyPlot.ylabel("q")
+    legend(loc="center left", title = "Mass (kg)",bbox_to_anchor=(1, 0.5))
+    PyPlot.savefig("./figures/changing_mass/$figname.png",transparent = true)
+
+    figname = "d_AOA"
+    PyPlot.figure(figname)
+    PyPlot.plot(savestates[:,1],alpha_save,".-",label = "$(round.(Mass,4))")
+    PyPlot.xlabel("pn (m)")
+    PyPlot.ylabel("AOA (deg)")
+    legend(loc="center left", title = "Mass (kg)",bbox_to_anchor=(1, 0.5))
+    PyPlot.savefig("./figures/changing_mass/$figname.png",transparent = true)
 
     figname = "d_Va"
     PyPlot.figure(figname)
-    PyPlot.plot(savestates[:,1],Va,".-",label = "$(round.(PW,4))")
+    PyPlot.plot(savestates[:,1],Va,".-",label = "$(round.(Mass,4))")
     PyPlot.xlabel("pn (m)")
     PyPlot.ylabel("Va (m/s)")
-    legend(loc="center left", title = "Power/Weight",bbox_to_anchor=(1, 0.5))
-    PyPlot.savefig("./figures/dynamic_opt/$figname.png",transparent = true)
+    legend(loc="center left", title = "Mass (kg)",bbox_to_anchor=(1, 0.5))
+    PyPlot.savefig("./figures/changing_mass/$figname.png",transparent = true)
     # PyPlot.ylim(0,32)
 
     figname = "pn_pd"
     PyPlot.figure(figname)
-    PyPlot.plot(savestates[:,1],-savestates[:,2],".-",label = "$(round.(PW,4))")
+    PyPlot.plot(savestates[:,1],-savestates[:,2],".-",label = "$(round.(Mass,4))")
     PyPlot.xlabel("pn (m)")
     PyPlot.ylabel("-pd (m)")
-    legend(loc="center left", title = "Power/Weight",bbox_to_anchor=(1, 0.5))
-    PyPlot.savefig("./figures/dynamic_opt/$figname.png",transparent = true)
+    legend(loc="center left", title = "Mass (kg)",bbox_to_anchor=(1, 0.5))
+    PyPlot.savefig("./figures/changing_mass/$figname.png",transparent = true)
+
+    figname = "pn_pd_norm"
+    PyPlot.figure(figname)
+    PyPlot.plot(savestates[:,1]/maximum(savestates[:,1]),-savestates[:,2]/maximum(-savestates[:,2]),".-",label = "$(round.(Mass,4))")
+    PyPlot.xlabel("pn/max pn")
+    PyPlot.ylabel("-pd/max -pd")
+    legend(loc="center left", title = "Mass (kg)",bbox_to_anchor=(1, 0.5))
+    PyPlot.savefig("./figures/changing_mass/$figname.png",transparent = true)
     # PyPlot.xlim(0,70)
     # PyPlot.ylim(0,20)
 
     figname = "t_T"
     PyPlot.figure(figname)
-    PyPlot.plot(time_sim,savestates[:,end]./(ones(time_sim)*P[:mass]*P[:gravity]),".-",label = "$(round.(PW,3))")
+    PyPlot.plot(time_sim,savestates[:,end]./(P[:mass]*P[:gravity]),".-",label = "$(round.(Mass,4))")
     # PyPlot.plot(time_sim,ones(time_sim)*P[:mass]*P[:gravity],"k-")
     PyPlot.xlabel("time (s)")
     PyPlot.ylabel("Thrust/Weight")
-    legend(loc="center left", title = "Power/Weight",bbox_to_anchor=(1, 0.5))
-    PyPlot.savefig("./figures/dynamic_opt/$figname.png",transparent = true)
+    legend(loc="center left", title = "Mass (kg)",bbox_to_anchor=(1, 0.5))
+    PyPlot.savefig("./figures/changing_mass/$figname.png",transparent = true)
 
     power = Va.*savestates[:,end]
 
-    figname = "power"
+    figname = "d_power"
     PyPlot.figure(figname)
-    PyPlot.plot(time_sim,power,".-",label = "$(round.(PW,4))")
+    PyPlot.plot(savestates[:,1],power./(P[:mass]*P[:gravity]),".-",label = "$(round.(Mass,4))")
     # PyPlot.plot(time_sim,ones(time_sim)*P[:mass]*P[:gravity],"k-")
-    PyPlot.xlabel("time (s)")
-    PyPlot.ylabel("Power")
-    legend(loc="center left", title = "Power/Weight",bbox_to_anchor=(1, 0.5))
-    PyPlot.savefig("./figures/dynamic_opt/$figname.png",transparent = true)
+    PyPlot.xlabel("pn (m)")
+    PyPlot.ylabel("Power/weight")
+    legend(loc="center left", title = "Mass (kg)",bbox_to_anchor=(1, 0.5))
+    PyPlot.savefig("./figures/changing_mass/$figname.png",transparent = true)
+
+    figname = "d_delta_e"
+    PyPlot.figure(figname)
+    PyPlot.plot(savestates[:,1],xopt[2:21],".-",label = "$(round.(Mass,4))")
+    # PyPlot.plot(time_sim,ones(time_sim)*P[:mass]*P[:gravity],"k-")
+    PyPlot.xlabel("pn (m)")
+    PyPlot.ylabel("Elevator Deflection (deg)")
+    legend(loc="center left", title = "Mass (kg)",bbox_to_anchor=(1, 0.5))
+    PyPlot.savefig("./figures/changing_mass/$figname.png",transparent = true)
 
     alt_spl = Dierckx.Spline1D(savestates[:,1],-savestates[:,2])
     va_spl = Dierckx.Spline1D(savestates[:,1],Va)
@@ -197,8 +271,16 @@ for i = 1:length(PW_array)
     t_spl = Dierckx.Spline1D(savestates[:,1],time_sim)
     P_spl = Dierckx.Spline1D(time_sim,power)
 
-    args = (alt_spl,h_set)
-    dist_at_height = fzero(zero_height,0,maximum(savestates[:,1]))
+    args2 = (alt_spl,h_set)
+    dist_at_height = 0.0
+    try
+        dist_at_height = fzero(zero_height,0,maximum(savestates[:,1]))
+    catch
+        dist_at_height = maximum(savestates[:,1])
+        if savestates[end,2]<h_set
+            warn("Max height was $(savestates[end,2]), should be $h_set")
+        end
+    end
     time_at_height = t_spl(dist_at_height)
     total_energy[i] = integrate(P_spl,0,time_at_height)
     final_dist[i] = dist_at_height
@@ -207,21 +289,21 @@ end
 
 figname = "errorsum"
 PyPlot.figure(figname)
-PyPlot.plot(PW_array,errorsum,".-",label = "$(round.(PW_array,3))")
-PyPlot.xlabel("Power/Weight")
+PyPlot.plot(Mass_array,errorsum,".-",label = "$(round.(Mass_array,3))")
+PyPlot.xlabel("Mass")
 PyPlot.ylabel("errorsum")
-PyPlot.savefig("./figures/dynamic_opt/$figname.png",transparent = true)
+PyPlot.savefig("./figures/changing_mass/$figname.png",transparent = true)
 
 figname = "total_energy"
 PyPlot.figure(figname)
-PyPlot.plot(PW_array,total_energy/(P[:mass]*P[:gravity]),".-",label = "$(round.(PW_array,3))")
-PyPlot.xlabel("Power/Weight")
+PyPlot.plot(Mass_array,total_energy/(P[:mass]*P[:gravity]),".-",label = "$(round.(Mass_array,3))")
+PyPlot.xlabel("Mass")
 PyPlot.ylabel("Energy/Weight (J/N)")
-PyPlot.savefig("./figures/dynamic_opt/$figname.png",transparent = true)
+PyPlot.savefig("./figures/changing_mass/$figname.png",transparent = true)
 
 figname = "average_slope"
 PyPlot.figure(figname)
-PyPlot.plot(PW_array,h_set./final_dist,".-",label = "$(round.(PW_array,3))")
-PyPlot.xlabel("Power/Weight")
+PyPlot.plot(Mass_array,h_set./final_dist,".-",label = "$(round.(Mass_array,3))")
+PyPlot.xlabel("Mass")
 PyPlot.ylabel("Average Slope (H/D)")
-PyPlot.savefig("./figures/dynamic_opt/$figname.png",transparent = true)
+PyPlot.savefig("./figures/changing_mass/$figname.png",transparent = true)
